3f7e7530b6f01392e9d1de1f35ad5a12ae775f6d
Vladimir <45636169+softmasterlab@users.noreply.github.com>
2020-12-15 18:53:52 +0200

    Исправления в .gitignore

--- DeliverySystem/Repositories/Catalog.cs
+++ DeliverySystem/Repositories/Catalog.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DeliverySystem.DataModels;
 
 namespace DeliverySystem.Repositories
 {
     class Catalog
     {
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public void Display()
+        {
+
+        }
     }
 }
